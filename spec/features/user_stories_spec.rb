decc469738f09e297fcb05d8f500b6819884094e
Nomi Vos <nvoswork@gmail.com>
2016-07-15 16:14:19 -0500

    adds spec/features/user_stories_spec.rb file and the start of the first test

---
+++ spec/features/user_stories_spec.rb
@@ -0,0 +1,10 @@
+describe User_stories do
+  # As a customer
+  # So that I can check if I want to order something
+  # I would like to see a list of dishes with prices
+  it 'to see if want to make an order, see a list of dishes with prices' do
+    restaurant = Restaurant.new
+    menu = Menu.new
+    expect { restaurant.list(menu) }.not_to raise_error
+  end
+end

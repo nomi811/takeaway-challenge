ee9ab0c3af7623393338e80686e6a9c56e0d10a3
Nomi Vos <nvoswork@gmail.com>
2016-07-16 15:15:44 -0500

    finishes User story 4 setting up Twilio

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,12 +2,18 @@ require 'takeaway'
 
 describe Takeaway do
 
-  subject(:takeaway) { described_class.new(menu: menu, order: order) }
+  subject(:takeaway) { described_class.new(menu: menu, order: order, sms: sms, config: {}) }
 
   let(:menu) { double(:menu, prints: printed_menu) }
   let(:order) { instance_double("Order", total: 9.00) }
+  let(:sms) { instance_double("SMS", deliver: nil) }
   let(:printed_menu) {'sandwich $4.50'}
+
   let(:dishes) { {sandwich: 2, soup: 2}}
+
+  before do
+    allow(order).to receive(:add)
+  end
 
     it 'shows the menu' do
       expect(takeaway.print_menu).to eq(printed_menu)
@@ -19,8 +25,12 @@ describe Takeaway do
     end
 
     it 'knows the order total' do
-      allow(order).to receive(:add)
       total = takeaway.place_order(dishes)
       expect(total).to eq(9.00)
+    end
+
+    it 'sends an SMS when the order has been placed' do
+      expect(sms).to receive(:deliver)
+      takeaway.place_order(dishes)
     end
 end
